fb27d5466b7b62635fe96aad75dc49ddfa3e8de4
Daniel Wang <danielwpz@gmail.com>
2022-01-01 14:18:15 +0800

    styles

--- src/pages/MyNFTS/Reveal.jsx
+++ src/pages/MyNFTS/Reveal.jsx
@@ -2,19 +2,20 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
+import boxGif from '../../assets/img/box.gif';
 
 const Reveal = ({ handleClick, title }) => (
   <div className="reveal__item" onClick={handleClick}>
     <div className="reveal__top">
       <picture>
-        <source srcSet="./images/reveal-2x.webp 2x, ./images/reveal.webp 1x" />
-        <img className="reveal__image" src="./images/reveal.png" alt="reveal" />
+        {/* <source srcSet="./images/reveal-2x.webp 2x, ./images/reveal.webp 1x" />
+        <img className="reveal__image" src="./images/reveal.png" alt="reveal" /> */}
+        <img className="reveal__image" src={boxGif} alt="nft" />
       </picture>
 
       <div className="reveal__question-sign">
-        ?<span className="reveal__question-sign-big">?</span>?
       </div>
-      <div className="reveal__text-hover">Reveal</div>
+      <div className="reveal__text-hover">打开</div>
     </div>
     <div className="reveal__title">
       <span># {title.padStart(5, '0')}</span>
